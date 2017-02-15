4a19af55075b3119f7a657d772a0315e58707f74
Sophie McDonald <sophie-rose23@hotmail.com>
2017-02-15 13:13:29 +1100

    Moved to postgres for our DB

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,14 @@
 class EventsController < ApplicationController
   def new
-    @events = Event.new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.create(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
