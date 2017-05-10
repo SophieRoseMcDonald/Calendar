a346a32765309436b4a8f1cc7d0741cd6d329d63
Sophie <sophiermcdonald@gmail.com>
2017-05-10 10:26:43 +1000

    Initial look at calendar feature

---
+++ features/calendar.feature
@@ -0,0 +1,13 @@
+Feature: The calendar page
+
+  Scenario: A calendar with no events
+    Given a user called "sophie@test.com" with a password of "password"
+    And I log in
+    When I go to the calendar page
+    Then I should not see "my event"
+
+  Scenario: A calendar with an event
+    Given a user called "sophie@test.com" with a password of "password"
+    And I log in
+    When I go to the calendar page
+    Then I should see "my event"
---
+++ features/step_definitions/calendar_steps.rb
@@ -0,0 +1,19 @@
+Given(/^a user called "([^"]*)" with a password of "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I log in$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I go to the calendar page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should not see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
